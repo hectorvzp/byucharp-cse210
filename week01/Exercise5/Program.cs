c17c72baf1227062b2096453723c62b953f6d872
hectorvzp <elderzambranoperez@gmail.com>
2025-03-18 15:17:12 -0300

    week1

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 class Program
 {
@@ -6,33 +7,38 @@ class Program
     {
         static string DisplayWelcome()
         {
-            string welcomee = ("Welcome to the program!");
-            console.WriteLine(welcomee);
+            string welcomee = "Welcome to the program!";
+            Console.WriteLine(welcomee);
             return welcomee;
         }
-        static int PromptUserNumber()
+        static int UserNumber()
         {
-            console.Write("What is your  favorite number?");
-            string number = console.ReadLine();
-            int numberInt = int.parse(number)
+            Console.Write("What is your  favorite number? ");
+            string number = Console.ReadLine();
+            int numberInt = int.Parse(number);
             return numberInt;
         }
         static string PromptUserName()
         {
-            console.Write("What is your  name?");
-            string name = console.ReadLine();
+            Console.Write("What is your  name? ");
+            string name = Console.ReadLine();
             return name;
         }
         static int SquareNumber(int number)
         {
-            int number = number * number
+            number *= number;
             return number;
         }
         static void DisplayResult(string name, int number)
         {
-            console.WriteLine($"Brother {name}, the square of your number is {number}")
+            Console.WriteLine($"Brother {name}, the square of your number is {number}");
         }
 
-    DisplayWelcome()
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = UserNumber();
+        int sq = SquareNumber(number);
+        DisplayResult(name, sq);
+
     }
 }
