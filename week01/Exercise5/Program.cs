65ffdd82b8cceaf2fa050433fa46590f59354bde
hectorvzp <elderzambranoperez@gmail.com>
2025-03-18 14:42:34 -0300

    df

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        static string DisplayWelcome()
+        {
+            string welcomee = ("Welcome to the program!");
+            console.WriteLine(welcomee);
+            return welcomee;
+        }
+        static int PromptUserNumber()
+        {
+            console.Write("What is your  favorite number?");
+            string number = console.ReadLine();
+            int numberInt = int.parse(number)
+            return numberInt;
+        }
+        static string PromptUserName()
+        {
+            console.Write("What is your  name?");
+            string name = console.ReadLine();
+            return name;
+        }
+        static int SquareNumber(int number)
+        {
+            int number = number * number
+            return number;
+        }
+        static void DisplayResult(string name, int number)
+        {
+            console.WriteLine($"Brother {name}, the square of your number is {number}")
+        }
+
+    DisplayWelcome()
     }
 }
